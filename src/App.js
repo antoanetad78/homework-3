57249ba5bde1d444c5cb16ff14250af384a74c09
antoanetad78 <antoanetad78@gmail.com>
2019-03-02 10:17:12 +0100

    Step 1 - done. Select and options added in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const data = { "Ivel Z3": { manufacturer: "Ivasim", year: 1969, origin: "Croatia" },
+ "Bally Astrocade": { manufacturer: "Bally Consumer Products", year: 1977, origin: "USA" },
+  "Sord M200 Smart Home Computer": { manufacturer: "Sord Computer Corporation", year: 1971, origin: "Japan" },
+   "Commodore 64": { manufacturer: "Commodore", year: 1982, origin: "USA" } }
+
+  
+const dataKeys = Object.keys(data);
+const dataToArray = dataKeys.map((element) => {
+  return {name:element,year: data[element].year}
+})
+
+console.log(dataToArray);
+
+
 class App extends Component {
+
+  
   render() {
     return (
       <div className="App">
-        
+        <select>
+          <option value=''>--Pick a model--</option>
+          {dataToArray.map(model=><option key={model.name} value={model.name}>{`${model.name} (${model.year})`}</option>)}
+        </select>
       </div>
     );
   }
